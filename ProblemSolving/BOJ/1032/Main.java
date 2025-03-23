7c79ce0a21baa48e85afe1ca89f4b0f28e7587f8
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-03-23 20:16:46 +0900

    1032

---
+++ ProblemSolving/BOJ/1032/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class J1032{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        String[] dirs = new String[n];
+
+        for(int i = 0; i < n; i++){
+            dirs[i] = scan.next();
+        }
+
+        String result = dirs[0];
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < result.length(); j++){
+                if(result.charAt(j) != dirs[i].charAt(j)){
+                    result = result.substring(0, j) + "?" + result.substring(j + 1);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
