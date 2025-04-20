da4a2bcc0ca203480d865930ffe046f3a1b53b5a
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-04-21 01:57:24 +0900

    1402

--- ProblemSolving/BOJ/1402/1402.py
+++ ProblemSolving/BOJ/1402/1402.py
@@ -5,3 +5,7 @@ T = int(sys.stdin.readline().rstrip())
 for i in range(T):
     A, B = map(int, sys.stdin.readline().rstrip().split())
     print('yes')
+    
+## 6 5
+# 6 = 6 * (-1) * (-1) * (1)
+# 5 = 6 - 1 - 1 + 1
