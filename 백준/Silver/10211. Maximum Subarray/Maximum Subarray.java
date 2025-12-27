b0e42bd6724965c23ba9a8e9f027fe2af052c199
gguip1 <gguip7554@naver.com>
2025-12-27 18:09:03 +0900

    [Silver IV] Title: Maximum Subarray, Time: 128 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
@@ -2,36 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] a;
+    static int[] array;
     static int[] memo;
-    static boolean[] vis;
+    static boolean[] visited;
 
     static int bestEnd(int i) {
-        if (vis[i]) return memo[i];
-        vis[i] = true;
+        if (visited[i]) return memo[i];
+        visited[i] = true;
 
-        if (i == 0) return memo[i] = a[0];
-        return memo[i] = Math.max(a[i], a[i] + bestEnd(i - 1));
+        if (i == 0) return memo[i] = array[0];
+        return memo[i] = Math.max(array[i], array[i] + bestEnd(i - 1));
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 0; tc < T; tc++) {
+        for (int i = 0; i < T; i++){
             int N = Integer.parseInt(br.readLine());
-            a = new int[N];
+            array = new int[N];
             memo = new int[N];
-            vis = new boolean[N];
+            visited = new boolean[N];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) a[i] = Integer.parseInt(st.nextToken());
-
-            int ans = Integer.MIN_VALUE;
-            for (int i = 0; i < N; i++) {
-                ans = Math.max(ans, bestEnd(i));
+            int idx = 0;
+            while (st.hasMoreTokens()) {
+                array[idx++] = Integer.parseInt(st.nextToken());
             }
-            System.out.println(ans);
+
+            int total_max = -9999;
+            for (int j = 0; j < N; j++){
+                total_max = Math.max(total_max, bestEnd(j));
+            }
+
+            System.out.println(total_max);
         }
     }
 }
