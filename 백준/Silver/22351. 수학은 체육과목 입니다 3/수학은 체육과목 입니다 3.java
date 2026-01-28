2a3019835a84a01607bdffff8671eedbc600c7cf
gguip1 <gguip7554@naver.com>
2026-01-28 20:26:07 +0900

    [Silver IV] Title: 수학은 체육과목 입니다 3, Time: 160 ms, Memory: 17236 KB -BaekjoonHub

---
+++ 백준/Silver/22351. 수학은 체육과목 입니다 3/수학은 체육과목 입니다 3.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static String S;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = br.readLine();
+
+        for (int i = 1; i <= 1000; i++) {
+            int num = i;
+            int index = 0;
+
+            while (index < S.length()) {
+                String numStr = Integer.toString(num);
+
+                if (S.length() < index + numStr.length()) {
+                    break;
+                }
+
+                if (S.startsWith(numStr, index)) {
+                    index += numStr.length();
+                    num++;
+                } else {
+                    break;
+                }
+
+                if (index == S.length()) {
+                    System.out.println(i + " " + (num - 1));
+                    System.exit(0);
+                }
+            }
+        }
+    }
+}
