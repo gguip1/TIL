39e9feae999ad4a24d713b7de54d2d3bb0f3e897
gguip1 <gguip7554@naver.com>
2026-02-08 15:26:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 528 ms, Memory: 49780 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int V, E;
+    static int[] P;
+    static Edge[] edges;
+
+    static int find(int a) {
+        if (P[a] < 1) {
+            return a;
+        }
+        return P[a] = find(P[a]);
+    }
+
+    static boolean union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return false;
+        }
+        P[a] = b;
+        return true;
+    }
+
+    static class Edge{
+        int A, B, C;
+        public Edge(int A, int B, int C) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        P = new int[V + 1];
+        edges = new Edge[E];
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            edges[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(edges, Comparator.comparingInt(e -> e.C));
+
+        int count = 0;
+        int answer = 0;
+
+        for (int i = 0; i < E; i++) {
+            if (!union(edges[i].A, edges[i].B)) {
+                continue;
+            }
+
+            answer += edges[i].C;
+            count++;
+            if (count == V - 1) {
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
