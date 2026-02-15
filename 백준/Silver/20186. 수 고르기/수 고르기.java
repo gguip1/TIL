0bd6ae3e5eb542c00967d244c0762e96154341be
gguip1 <gguip7554@naver.com>
2026-02-15 14:28:32 +0900

    [Silver IV] Title: 수 고르기, Time: 124 ms, Memory: 15584 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/20186. 수 고르기/수 고르기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for (int i = 0; i < K; i++) {
+            answer += arr[N - 1 - i] - i;
+        }
+        System.out.println(answer);
+    }
+}
