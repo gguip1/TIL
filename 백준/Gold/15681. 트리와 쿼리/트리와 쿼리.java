d2d1faa92ce746d00053aee4358a675b269d3125
gguip1 <gguip7554@naver.com>
2026-04-12 15:47:41 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1280 ms, Memory: 81452 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, R, Q;
+    static List<List<Integer>> tree;
+    static int[] dp;
+
+    static int dfs(int root) {
+        dp[root] = 1;
+
+        for (int n : tree.get(root)) {
+            if (dp[n] == 0) {
+                dp[root] += dfs(n);
+            }
+        }
+
+        return dp[root];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        tree = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        dp = new int[N + 1];
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            tree.get(U).add(V);
+            tree.get(V).add(U);
+        }
+
+        dfs(R);
+
+        for (int i = 0; i < Q; i++) {
+            int root = Integer.parseInt(br.readLine());
+            System.out.println(dp[root]);
+        }
+    }
+}
